203c9574353543331076fa821242afa1ffb3d6c6
Andres Rodriguez <anrocox@gmail.com>
2014-07-03 18:53:12 -0500

    In python, how do I know if an item exists in a list?

---
+++ src/exists_item_lists.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on Jul 2, 2014
+
+@author: anroco
+
+In python, how do I know if an item exists in a list?
+
+En python, ¿Cómo se puede saber si un elemento existe en una lista?
+'''
+
+#create a list
+lista = [1, 'hello', True, 'a', 2.2]
+print (lista)
+
+#use the in statement
+print('hello' in lista)
+print(True in lista)
+print(2 in lista)
